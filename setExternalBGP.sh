d0d9698c019699d5c81ff80a1011012569d354ac
otavio <oaugusto@outlook.com>
2018-07-03 12:52:03 -0400

    new function

--- setExternalBGP.sh
+++ setExternalBGP.sh
@@ -16,6 +16,13 @@ setPeer() {
         -c \"neighbor $2 remote-as $3\"" | ./bash-in.sh $1
 }
 
+publish() {
+    echo "vtysh \
+        -c \"conf t\" \
+	-c \"router bgp 15\" \
+        -c \"network $2\"" | ./bash-in.sh $1
+}
+
 #SEAT
 echo "SEAT"
 
@@ -24,6 +31,8 @@ setIP SEAT ebgp "179.24.49.15/24"
 
 setPeer SEAT "179.24.23.6" 6 
 setPeer SEAT "179.24.23.19" 19
+
+publish SEAT "15.0.0.0/8"
 
 #LOSA
 echo "LOSA"
@@ -34,6 +43,8 @@ setIP LOSA ebgp "179.24.49.15/24"
 setPeer LOSA "179.24.23.6" 6
 setPeer LOSA "179.24.49.19" 19
 
+publish LOSA "15.0.0.0/8"
+
 #SALT
 echo "SALT"
 
@@ -42,6 +53,8 @@ setIP SALT ebgp "179.24.45.15/24"
 
 setPeer SALT "179.24.36.11" 11
 setPeer SALT "179.24.45.14" 14
+
+publish SALT "15.0.0.0/8"
 
 #KANS
 echo "KANS"
@@ -52,12 +65,16 @@ setIP KANS ebgp "179.24.48.15/24"
 setPeer KANS "179.24.33.10" 10
 setPeer KANS "179.24.48.20" 20
 
+publish KANS "15.0.0.0/8"
+
 #HOUS
 echo "HOUS"
 
 setIP HOUS ebgp "190.0.0.15/24"
 
 setPeer HOUS "190.0.0.99" 99
+
+publish HOUS "15.0.0.0/8"
 
 #WASH
 echo "WASH"
@@ -68,6 +85,8 @@ setIP WASH ebgp "179.24.36.15/24"
 setPeer WASH "179.24.45.14" 14
 setPeer WASH "179.24.36.11" 11
 
+publish WASH "15.0.0.0/8"
+
 #NEWY
 echo "NEWY"
 
@@ -76,3 +95,5 @@ setIP NEWY ebgp "179.24.48.15/24"
 
 setPeer NEWY "179.24.33.10" 10
 setPeer NEWY "179.24.48.20" 20
+
+publish NEWY "15.0.0.0/8"
